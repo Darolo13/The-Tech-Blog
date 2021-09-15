1f18cfdeacfb7b69243975d8a9b2e30ec203cf1c
Darolo13 <70618345+Darolo13@users.noreply.github.com>
2021-09-15 08:12:37 -0700

    Users can create, read, update, and delete a profile in the database

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const routes = require('./routes');
 const sequelize = require('./config/connection');
 
 const app = express();
@@ -7,6 +8,9 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-sequelize.sync({ force: false }).then(() => {
+// turn on routes
+app.use(routes);
+
+sequelize.sync({ force: true }).then(() => {
     app.listen(PORT, () => console.log('Now listening!'));
 });

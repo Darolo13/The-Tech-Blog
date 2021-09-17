069427c7290873cfe0040670c71dd18705116207
Darolo13 <70618345+Darolo13@users.noreply.github.com>
2021-09-17 00:15:38 -0700

    Create User seeds

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const { Post, User, Comment } = require('../models');
+const { Post, User } = require('../models');
 
 router.get('/', (req, res) => {
    Post.findAll({
@@ -10,20 +10,6 @@ router.get('/', (req, res) => {
            'title',
            'created_at',
            [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
-       ],
-       include: [
-           {
-               model: Comment,
-               attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-               include: {
-                   model: User,
-                   attributes: ['username']
-               }
-           },
-           {
-               model: User,
-               attributes: ['username']
-           }
        ]
    })
    .then(dbPostData => {

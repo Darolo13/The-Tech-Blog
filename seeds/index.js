5d361e9d927ded40bb4040490f6e1cebf97f89ad
Darolo13 <70618345+Darolo13@users.noreply.github.com>
2021-09-17 00:24:57 -0700

    Create Comment seeds

--- seeds/index.js
+++ seeds/index.js
@@ -1,5 +1,6 @@
 const userSeed = require('./user-seeds');
 const postSeed = require('./post-seeds');
+const commentSeed = require('./comment-seeds')
 
 const sequelize = require('../config/connection');
 
@@ -7,6 +8,7 @@ const completeSeed = async() => {
     await sequelize.sync({ force: true });
     await userSeed();
     await postSeed();
+    await commentSeed();
     process.exit(0);
 };
 

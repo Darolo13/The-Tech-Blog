bd1143219c520e33340184ceaa3267134ae89e30
Darolo13 <70618345+Darolo13@users.noreply.github.com>
2021-09-17 00:41:47 -0700

    Refactor models

--- models/User.js
+++ models/User.js
@@ -20,18 +20,7 @@ User.init(
         username: {
             type: DataTypes.STRING,
             allowNull: false,
-            unique: true,
-            validate: {
-                isEmail: true
-            }
-        },
-        email: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            unique: true,
-            validate: {
-                isEmail: true
-            }
+            unique: true
         },
         password: {
             type: DataTypes.STRING,
@@ -63,6 +52,3 @@ User.init(
 );
 
 module.exports = User;
-
-
-// left it on fixing the back end jawsdb
